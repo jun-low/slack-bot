d0ab5f0df50b5447379a2fe35869a8b8290bd6e5
Jun Low <zhijun.low@gmail.com>
2024-01-04 15:26:05 +1300

    add bot icon and api credentials

--- src/util/slack.ts
+++ src/util/slack.ts
@@ -1,1 +1,16 @@
-// TODO create Slack utilities
+import type { HandlerEvent } from '@netlify/functions';
+import { createHmac } from 'crypto';
+
+export function slackApi(
+	endpoint: SlackApiEndpoint,
+	body: SlackApiRequestBody,
+) {
+	return fetch(`https://slack.com/api/${endpoint}`, {
+		method: 'POST',
+		headers: {
+			Authorization: `Bearer ${process.env.SLACK_BOT_OAUTH_TOKEN}`,
+			'Content-Type': 'application/json; charset=utf-8',
+		},
+		body: JSON.stringify(body),
+	}).then((res) => res.json());
+}
